6d6efeef591ae460b44961a9a016beb837daeb99
kubohisa <kubohisa@gmail.com>
2025-06-07 07:12:52 +0900

    Update Router.php

--- Typoon/System/Router.php
+++ Typoon/System/Router.php
@@ -87,7 +87,7 @@ if (typoon::$Debug === true) {
 } else {
     ini_set('display_errors', 'Off');
     ini_set('display_startup_errors', 'Off');
-    ini_set('error_reporting', E_ALL);
+    ini_set('error_reporting', 0);
 }
 
 ini_set('log_errors', 'On');
