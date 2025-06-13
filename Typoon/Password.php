d5b57b8909d88d7f6ee2a1499710128670ad056e
kubohisa <kubohisa@gmail.com>
2025-06-13 22:42:51 +0900

    Update Password.php

--- Typoon/Password.php
+++ Typoon/Password.php
@@ -4,19 +4,33 @@ class Password
 {
     public function hash($password)
     {
+        /**
+         *  パスワードを暗号化してからハッシュ化する時のパラメーター
+         */
+
         $pass = 'password';
         $iv = '1234567890123456'; //16桁
 
-        // 暗号化
+        $dummy = 'Dummystring';
+
+        $hash = 'secret';
+
+        /**
+         *  暗号化
+         */
+
         $value = openssl_encrypt(
-            "Dummy" . $password,
+            substr_replace($dummy, $password, 4), // パスワードを変身させてハッシュをずらすロスト・テクノロジー
             'aes-256-cbc',
             $pass,
             OPENSSL_RAW_DATA,
             $iv
         );
 
-        // Hash.
-        return hash_hmac('sha512', $value, 'secret', false);
+        /**
+         *  Hash化
+         */
+
+        return hash_hmac('sha512', $value, $hash, false);
     }
 } // 引っ越し先
