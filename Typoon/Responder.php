d5f131cf7f2e568e1b1c623b74de2131b19708c7
kubohisa <kubohisa@gmail.com>
2025-06-16 17:25:37 +0900

    Update Responder.php
    Location処理を追加

--- Typoon/Responder.php
+++ Typoon/Responder.php
@@ -2,10 +2,36 @@
 
 class Responder
 {
+    /**
+     * JSON出力
+     */
+
     public static function json($data)
     {
         header("Content-Type: application/json; charset=UTF-8");
         echo json_encode($data);
+
+        exit;
+    }
+
+    /**
+     * Location処理
+     */
+
+    public static function location($url)
+    {
+        /**
+         * URLチェック.
+         */
+        if (! filter_var($url, FILTER_VALIDATE_URL)) {
+            Std::errorPage(503);
+        }
+
+        /**
+         * Location処理
+         */
+
+        header("Location: " . $url);
 
         exit;
     }
